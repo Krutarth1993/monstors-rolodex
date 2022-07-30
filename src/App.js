ea8c9fd5ed683173ef1772507a8f5cceba0b41f2
Krutarth1993 <kpj3111993@gmail.com>
2022-07-29 21:24:00 -0400

    setting the state with secondary callback
    console & updating the state with the asyncronous way using the callback function.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,20 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>Hi {this.state.name}</p>
-          <button>Change-Name</button>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { name: "Vicky" };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change-Name
+          </button>
         </header>
       </div>
     );
